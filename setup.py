8294ee9b9b4388ef2edc4047532f4a83b1ea268b
Yan Guo <silveryy0528@gmail.com>
2026-08-05 15:33:32 +0200

    set up project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='rag',
+    version='0.0.1',
+    author='Yan Guo',
+    description='A simple RAG implementation',
+    url='https://github.com/silveryy0528-coder/RAG',
+    packages=find_packages(),
+)
